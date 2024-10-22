0fa8b9209fc4418dc818994dad437b9e7d12d9e1
ian <ian@soulware.kr>
2024-10-22 17:50:56 +0900

    feat: android 빌드, ui 추가
    - build.gradle 문제 해결
    - firebase 로그인, 회원가입 ui 추가

---
+++ components/CustomButton.tsx
@@ -0,0 +1,48 @@
+import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
+
+function CustomButton({onPress, title, hasMarginBottom}) {
+  return (
+    <View
+      style={[
+        styles.block,
+        styles.overflow,
+        hasMarginBottom && styles.margin,
+      ]}>
+      <Pressable
+        onPress={onPress}
+        style={({pressed}) => [
+          styles.wrapper,
+          Platform.IOS === 'ios' && pressed && {opacity: 0.5},
+        ]}
+        android_ripple={{
+            color: '#ffffff',
+        }}>
+            <Text style={[styles.text]}>{title}</Text>
+        </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    overflow: {
+        borderRadius: 4,
+        overflow: 'hidden',
+    },
+    wrapper: {
+        borderRadius: 4,
+        height: 48,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#6200ee',
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 14,
+        color: 'white',
+    },
+    margin: {
+        marginBottom: 8,
+    },
+})
+
+export default CustomButton;
---
+++ screens/SignInScreen.tsx
@@ -0,0 +1,42 @@
+import {StyleSheet, Text, View} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import BorderedInput from '../components/BorderedInput';
+import CustomButton from '../components/CustomButton';
+
+function SignInScreen() {
+  return (
+    <SafeAreaView style={styles.fullscreen}>
+      <Text style={styles.text}>IanPublicGallery</Text>
+      <View style={styles.form}>
+        <BorderedInput hasMarginBottom />
+        <BorderedInput />
+        <View style={styles.buttons}>
+          <CustomButton title="로그인" hasMarginBottom />
+          <CustomButton title="회원가입" />
+        </View>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  fullscreen: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  form: {
+    marginTop: 64,
+    width: '100%',
+    paddingHorizontal: 16,
+  },
+  buttons: {
+    marginTop: 64,
+  },
+});
+
+export default SignInScreen;
